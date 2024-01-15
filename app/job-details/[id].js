631ed72a0bdac808f08424359d60bed6293a32e7
fakamda <facundoelcardenal@gmail.com>
2024-01-14 21:26:30 -0300

    company compoents

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -23,10 +23,17 @@ import useFetch from '../../hooks/useFetch'
 const JobDetails = () => {
   const params = useGlobalSearchParams()
   const router = useRouter()
+  
 
   const { data, isLoading, error, refetch } = useFetch('job-details', {
     job_id: params.id,
   })
+
+  const [refreshing, setRefreshing] = useState(false)
+
+  const onRefresh = () => {
+
+  }
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
@@ -51,6 +58,29 @@ const JobDetails = () => {
           headerTitle: '',
         }}
       />
+      <>
+        <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}>
+            { isLoading ? (
+                <ActivityIndicator size='large' color={COLORS.primary} />
+            ): error ? (
+                <Text>Something went wrong</Text>
+            ): data.length === 0 ? (
+                <Text>No Data</Text>
+            ): (
+                <View style={{ padding:SIZES.medium, paddingBottom: 100 }}>
+                    <Company
+                        companyLogo={data[0].employer_logo}
+                        jobTitle={data[0].job_title}
+                        companyName={data[0].employer_name}
+                        location={data[0].job_country}
+                    />
+                    <JobTabs
+                    
+                    />
+                </View>
+            ) }
+        </ScrollView>
+      </>
     </SafeAreaView>
   )
 }
