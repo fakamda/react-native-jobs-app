c023cfc7dd6eb1ed0771956b5da463b0eaffae2f
fakamda <facundoelcardenal@gmail.com>
2024-01-16 22:21:58 -0300

    a

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -3,10 +3,11 @@ import { View, Text } from 'react-native'
 
 import styles from './specifics.style'
 
-const Specifics = () => {
+const Specifics = ({title, points}) => {
   return (
-    <View>
-      <Text>Specifics</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}:</Text>
+      <View style={styles.pointsContainer}></View>
     </View>
   )
 }
